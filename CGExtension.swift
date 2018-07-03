5d51d8f08d7a42b4534c8ed72506d7b9973ee370
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-07-03 11:09:56 +0800

    Add View for card. Add some helper functions - extension to CG.

---
+++ CGExtension.swift
@@ -0,0 +1,39 @@
+//
+//  CGExtension.swift
+//  PlayingCard
+//
+//  Created by Svitlana Dzyuban on 3/7/18.
+//  Copyright © 2018 Lana Dzyuban. All rights reserved.
+//
+
+import UIKit
+
+extension CGRect {
+    var leftHalf: CGRect {
+        return CGRect(x: minX, y: minY, width: width/2, height: height)
+    }
+
+    var righttHalf: CGRect {
+        return CGRect(x: midX, y: minY, width: width/2, height: height)
+    }
+
+    func inset(by size: CGSize) -> CGRect{
+        return insetBy(dx: size.width, dy: size.height)
+    }
+
+    func sized(to size: CGSize) -> CGRect {
+        return CGRect(origin: origin, size: size)
+    }
+
+    func zoom(by scale: CGFloat) -> CGRect {
+        let newWidth = width * scale
+        let newHeight = height * scale
+        return insetBy(dx: (width - newWidth) / 2, dy: (height - newHeight) / 2)
+    }
+}
+
+extension CGPoint {
+    func offsetBy(dx: CGFloat, dy: CGFloat) -> CGPoint {
+        return CGPoint(x: x + dx, y: y + dy)
+    }
+}
---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,88 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Svitlana Dzyuban on 2/7/18.
+//  Copyright © 2018 Lana Dzyuban. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+    var rank: Int = 5 { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var suit: String = "♣️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var isFacedUp: Bool = true { didSet { setNeedsDisplay(); setNeedsLayout() } }
+
+    private var rankString: String {
+        switch rank {
+        case 1: return "A"
+        case 2...10: return String(rank)
+        case 11: return "J"
+        case 12: return "Q"
+        case 13: return "K"
+        default: return "?"
+        }
+    }
+
+    private struct SizeRatio {
+        static let cornerFontSizeToBoundsHeight: CGFloat = 0.085
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+        static let cornerOffsetToCornerRadius: CGFloat = 0.33
+        static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
+    }
+
+    private var cornerRadius: CGFloat {
+        return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
+    }
+
+    private var cornerOffset: CGFloat {
+        return cornerRadius * SizeRatio.cornerOffsetToCornerRadius
+    }
+
+    private var cornerFontSize: CGFloat {
+        return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight
+    }
+
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [.paragraphStyle: paragraphStyle, .font: font])
+    }
+
+    private var corneredString: NSAttributedString {
+        return centeredAttributedString("\(rankString)\n\(suit)", fontSize: cornerFontSize)
+    }
+
+    private lazy var upperLeftCornerLabel: UILabel = createCornerLabel()
+    private lazy var lowerRightCornerLabel: UILabel = createCornerLabel()
+
+    private func createCornerLabel() -> UILabel {
+        let label = UILabel()
+        label.numberOfLines = 0
+        addSubview(label)
+        return label
+    }
+
+    override func draw(_ rect: CGRect) {
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
+    }
+
+    private func cofigureCornerLabel(_ label: UILabel) {
+        label.attributedText = corneredString
+        label.frame.size = CGSize.zero
+        label.sizeToFit()
+        label.isHidden = !isFacedUp
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        cofigureCornerLabel(upperLeftCornerLabel)
+        upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
+    }
+
+}
