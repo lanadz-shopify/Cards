e461d99b3f8f2de13dad3d7ef33d03325bef6909
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-08-02 10:46:41 +0800

    Remove geastures

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -11,33 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     var deck = PlayingCardDeck()
 
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFacedUp = !playingCardView.isFacedUp
-        default:
-            break
-        }
-    }
+//    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+//        switch sender.state {
+//        case .ended:
+//            playingCardView.isFacedUp = !playingCardView.isFacedUp
+//        default:
+//            break
+//        }
+//    }
 
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left,.right]
-            playingCardView.addGestureRecognizer(swipe)
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
+    @IBOutlet weak var playingCardView: PlayingCardView! //{
+//        didSet {
+//            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+//            swipe.direction = [.left,.right]
+//            playingCardView.addGestureRecognizer(swipe)
+//            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+//            playingCardView.addGestureRecognizer(pinch)
+//        }
+//    }
 
-    @objc private func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
-        }
-
-    }
+//    @objc private func nextCard() {
+//        if let card = deck.draw() {
+//            playingCardView.rank = card.rank.order
+//            playingCardView.suit = card.suit.rawValue
+//        }
+//
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+            }
 }
