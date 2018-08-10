66bab16d589222d0d718336434768b65a4940c91
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-08-10 10:57:28 +0800

    Extension to CGFloat - random

--- PlayingCard/Int.swift
+++ PlayingCard/Int.swift
@@ -5,7 +5,7 @@
 //  Created by Svitlana Dzyuban on 29/6/18.
 //  Copyright © 2018 Lana Dzyuban. All rights reserved.
 //
-
+import UIKit
 import Foundation
 
 extension Int {
@@ -14,6 +14,18 @@ extension Int {
             return Int(arc4random_uniform(UInt32(self)))
         } else if self < 0 {
             return Int(arc4random_uniform(UInt32(-self)))
+        } else {
+            return 0
+        }
+    }
+}
+
+extension CGFloat {
+    var arc4random: CGFloat {
+        if self > 0 {
+            return CGFloat(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return CGFloat(arc4random_uniform(UInt32(-self)))
         } else {
             return 0
         }
